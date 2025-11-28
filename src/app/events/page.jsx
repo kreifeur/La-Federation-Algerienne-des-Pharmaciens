e7ebb1f3666b7ce4b1c77c15fc0a278a8ef3cc6b
kreifeur <kreifeur669@gmail.com>
2025-11-29 00:33:40 +0100

    events

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -414,12 +414,12 @@ export default function Events() {
                       <p className="text-gray-700 mb-4">{event.description}</p>
 
                       {/* Informations sur les participants */}
-                      <div className="flex items-center text-sm text-gray-500 mb-2">
+                     {/*  <div className="flex items-center text-sm text-gray-500 mb-2">
                         <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
                         <span>{participantsCount}/{maxParticipants} participants</span>
-                      </div>
+                      </div> */}
 
                       <div className="flex justify-between items-center mb-4">
                         <div className="flex items-center text-sm text-gray-600">
@@ -433,9 +433,9 @@ export default function Events() {
                             </span>
                           ) : (
                             <>
-                              <div className="text-gray-500 line-through text-sm">
+                              {/* <div className="text-gray-500 line-through text-sm">
                                 {event.nonMemberPrice} DA
-                              </div>
+                              </div> */}
                               <div className="text-blue-800 font-semibold">
                                 {event.memberPrice} DA membres
                               </div>
