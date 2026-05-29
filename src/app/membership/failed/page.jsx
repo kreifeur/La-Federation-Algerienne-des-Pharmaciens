7449c05abf2728337ffd58e91d70a9cf90aba3c9
kreifeur <kreifeur669@gmail.com>
2026-05-29 18:49:31 +0100

    add satim

--- src/app/membership/failed/page.jsx
+++ src/app/membership/failed/page.jsx
@@ -243,23 +243,9 @@ function PaymentFailedContent() {
                 <div className="flex flex-col sm:flex-row gap-2">
                   <a
                     href="tel:3020"
-                    className="text-center bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors text-sm font-medium flex-1"
+                    className="text-center text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors text-sm font-medium flex-1"
                   >
-                    <div className="flex items-center justify-center">
-                      <svg
-                        className="w-4 h-4 mr-2"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                        />
-                      </svg>
-                       <img
+                   <img
                           src="/satim.jpg"
                           alt="SATIM Payment Gateway"
                           className="h-10 mr-3"
@@ -267,7 +253,6 @@ function PaymentFailedContent() {
                             e.target.onerror = null;
                             e.target.style.display = "none";
                           }}/>
-                    </div>
                   </a>
                   <a
                     href="mailto:support@federation-pharmaciens.dz"
