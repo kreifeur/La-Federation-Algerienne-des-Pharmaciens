1e2f2eb6ab4b246eff9bbce248b5bd5d030789c3
kreifeur <kreifeur669@gmail.com>
2026-05-30 13:55:30 +0100

    add minutes in payment succees

--- src/app/payment/success/page.jsx
+++ src/app/payment/success/page.jsx
@@ -748,7 +748,7 @@ function RegisterSuccessContent() {
                     </div>
                     <div>
                       <p className="text-sm text-slate-500 mb-1">Date de transaction</p>
-                      <p className="font-medium">{now}</p>
+                      <p className="font-medium">{fullDate}</p>
                     </div>
                     <div>
                       <p className="text-sm text-slate-500 mb-1">Numéro d'autorisation</p>
